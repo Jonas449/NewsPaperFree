b6fd276e168bde44d98dda1a6594148bb7dbe26a
Jonas <hahn-jonas@gmx.net>
2021-02-24 14:44:38 +0100

    Added comments and addon works now on mobile

--- newspaperfree.js
+++ newspaperfree.js
@@ -5,15 +5,20 @@ function hidePaidContent() {
 hidePaidContent();
 
 function setText() {
+    // Get text
     let getInnerHtml = document.querySelector(".pdb-article script").innerHTML;
     let parseInnerHtml = JSON.parse(getInnerHtml);
     let getArticleBody = parseInnerHtml.articleBody;
-    let newContent = document.getElementsByClassName("pdb-article-body-paidcontentintro")[0];
+    // Remove article preview text
     let richText = document.getElementsByClassName("pdb-richtext-field")[1];
     richText.style.display = "none";
+    // Change height for mobile website
+    document.getElementsByClassName("pdb-article-body-paidcontentintro")[0].style.height = "auto";
+    // Create new div and append it
     let div = document.createElement("div");
     div.className = "news-article";
     div.textContent = getArticleBody;
+    let newContent = document.getElementsByClassName("pdb-article-body-paidcontentintro")[0];
     newContent.append(div);
 }
 setText();
